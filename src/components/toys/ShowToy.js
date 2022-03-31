3ca755c2be8c4a2cbf91f38feb3efebcab6d0e77
kaiguy920 <smithkai920@gmail.com>
2022-03-31 16:57:01 -0400

    show toys have colors

--- src/components/toys/ShowToy.js
+++ src/components/toys/ShowToy.js
@@ -2,11 +2,32 @@ import React from 'react'
 import { Card } from 'react-bootstrap'
 
 const ShowToy = (props) => {
-    console.log('props.toy', props.toy);
+    const { toy } = props
+
+    const setBgCondition = (cond) => {
+        if (cond === 'new') {
+            return { width: '18rem', backgroundColor: 'green' }
+        } else if (cond === 'used') {
+            return { width: '18rem', backgroundColor: 'yellow' }
+        } else {
+            return { width: '18rem', backgroundColor: 'red' }
+        }
+    }
+
+    console.log(setBgCondition(toy.condition))
 
     return (
-        <Card className="m-2" style={{ width: '18rem' }}>
-            <Card.Header>{props.toy.name}</Card.Header>
+        <Card className="m-2" style={setBgCondition(toy.condition)}>
+            <Card.Header>{toy.name}</Card.Header>
+            <Card.Body>
+                <small>{toy.description}</small><br />
+                <small>
+                    {toy.isSqueaky ? 'squeak squeak' : 'stoic silence'}
+                </small><br />
+                <Card.Footer >
+                    {toy.condition}
+                </Card.Footer>
+            </Card.Body>
         </Card>
     )
 }
