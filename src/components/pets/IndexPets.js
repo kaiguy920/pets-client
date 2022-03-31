acd130e6d0d84e4b05742529d3fc3fa200de0c55
kaiguy920 <smithkai920@gmail.com>
2022-03-31 10:11:58 -0400

    updates header and index

--- src/components/pets/IndexPets.js
+++ src/components/pets/IndexPets.js
@@ -1,5 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { getAllPets } from '../../api/pets'
+import { Card } from 'react-bootstrap'
+
+// I'm going to declare a style object
+// this will be used to corral my cards
+// we can use basic CSS, but we have to use JS syntax
+const cardContainerLayout = {
+    display: 'flex',
+    justifyContent: 'center',
+    flexFlow: 'row wrap'
+}
 
 const IndexPets = (props) => {
     const [pets, setPets] = useState(null)
@@ -18,22 +28,32 @@ const IndexPets = (props) => {
         return <p>no pets yet, go add some</p>
     }
 
-    let petsJsx
+    let petCards
 
     if (pets.length > 0) {
-        petsJsx = pets.map(pet => (
-            <li key={pet.id}>
-                {pet.fullTitle}
-            </li>
+        // petsJsx = pets.map(pet => (
+        //     <li key={pet.id}>
+        //         {pet.fullTitle}
+        //     </li>
+        // ))
+        petCards = pets.map(pet => (
+            // one method of styling, usually reserved for a single style
+            // we can use inline, just like in html
+            <Card key={pet.id} style={{ width: '30%' }} className="m-2" >
+                <Card.Header>{pet.fullTitle}</Card.Header>
+                <Card.Body>
+                    <Card.Text>{pet.name}</Card.Text>
+                </Card.Body>
+            </Card>
         ))
     }
 
     return (
         <>
             <h3>All the pets</h3>
-            <ul>
-                {petsJsx}
-            </ul>
+            <div style={cardContainerLayout}>
+                {petCards}
+            </div>
         </>
     )
 }
